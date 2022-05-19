0f74c65f65cf8cf078c6cd159280a031f5f692a8
Julio Lira <jlira@estrasol.com.mx>
2022-05-18 23:12:25 -0500

    add test e2e

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log('server started on port 5000') //eslint-disable-line no-console
+  console.log(`server started on port ${PORT}`) //eslint-disable-line no-console
 })
